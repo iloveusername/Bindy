6055395d1a1e5913f01b6ca1c479edc3ed13003c
iloveusername <bchasestair@gmail.com>
2023-01-30 07:06:52 -0800

    Actually based

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import os
 import pyaudio
+import pickle
+import AppOpener as ao
 import pyautogui as pg
 import tkinter as tk
 from vosk import Model, KaldiRecognizer
+
+
+class Command(object):
+    def __init__(self, name, keys):
+        self.name = name
+        self.keys = keys
 
 model = Model('vosk-model-small-en-us-0.15')
 recog = KaldiRecognizer(model, 16000)
@@ -9,12 +18,20 @@ recog = KaldiRecognizer(model, 16000)
 mic = pyaudio.PyAudio()
 stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
 
+
+
+computerName = 'computer'
 previousText = ''
 while True:
     data = stream.read(4096, exception_on_overflow=False)
     if recog.AcceptWaveform(data):
         text = recog.Result()
         text = text[14:-3]
-        print(text)
-        if text == 'computer open task manager':
+        if computerName + ' task manager' in text:
             pg.hotkey('ctrl','shift','esc')
+        if computerName + ' open ' in text:
+            text = text[text.index(' open ')+6:len(text)]
+            ao.open(text, match_closest=True)
+        if computerName + ' close ' in text:
+            text = text[text.index(' close ')+6:len(text)]
+            ao.close(text)
